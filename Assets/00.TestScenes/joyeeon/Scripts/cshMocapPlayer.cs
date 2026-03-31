7e83356aebc1d3c0cff5f5ef9970a27c950d64a6
조희연 <146899497+joyeeon@users.noreply.github.com>
2026-03-31 14:12:50 +0900

    Photon Mocap v0.1

--- Assets/00.TestScenes/joyeeon/Scripts/cshMocapPlayer.cs
+++ Assets/00.TestScenes/joyeeon/Scripts/cshMocapPlayer.cs
@@ -6,13 +6,13 @@ public class cshMocapPlayer : MonoBehaviourPun
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (photonView.IsMine) {
+
+            GameObject client = GameObject.Find("Client - OptiTrack");
+            GetComponent<OptitrackSkeletonAnimator>().StreamingClient = client.GetComponent<OptitrackStreamingClient>();
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (photonView.IsMine)
-            FindAnyObjectByType<OptitrackStreamingClient>().gameObject.SetActive(true);
-    }
+    
 }
