796a08f36d0a4bd0e05b24cc5c3a5e1391437df6
조희연 <146899497+joyeeon@users.noreply.github.com>
2026-03-31 16:57:11 +0900

    Feat : PUN Setting

--- Assets/00.TestScenes/joyeeon/Scripts/cshVRPlayer.cs
+++ Assets/00.TestScenes/joyeeon/Scripts/cshVRPlayer.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using Photon.Pun;
 
-public class cshVRPlayer : MonoBehaviour
+public class cshVRPlayer : MonoBehaviourPun
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (!photonView.IsMine)
+        {
+           // GameObject[] Camera = GameObject.FindGameObjectsWithTag("Camera");
+           //foreach(GameObject cam in Camera)
+           // {
+           //     cam.SetActive(false);
+           // }
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //void Update()
+    //{
+    //    photonView.RPC("addScore", RpcTarget.AllBuffered);
+    //}
+    //[PunRPC]
+    //public void addScore()
+    //{
+    //    Debug.Log("get Score!");
+    //}
 }
