b1a1a769fd134723eccf2a6fa456f8eadb1c98c9
SonDaHyeL <dajudysky@naver.com>
2026-05-13 21:57:43 +0900

    Fix : 씬1 조정

--- Assets/00.TestScenes/SonDaHyeL/Scripts/Editing/Scene1Controller.cs
+++ Assets/00.TestScenes/SonDaHyeL/Scripts/Editing/Scene1Controller.cs
@@ -27,6 +27,7 @@ public class Scene1Controller : AllSceneController
         // 1. 영상 재생
         bool videoDone = false;
         videoPlayer.onComplete = () => videoDone = true;
+        yield return new WaitForSeconds(2.5f);
         yield return StartCoroutine(soundManager.PlayAndWait("Scene1_1", 0.4f));
         yield return new WaitUntil(() => videoDone);
         yield return VideoFadeTransition();
