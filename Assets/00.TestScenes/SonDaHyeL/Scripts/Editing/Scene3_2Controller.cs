9504f3cedbe759c2609c406c214f9fe1c33d5275
승윤 <115918179+seungyun27@users.noreply.github.com>
2026-05-18 18:58:12 +0900

    fix:Scene3Changer

--- Assets/00.TestScenes/SonDaHyeL/Scripts/Editing/Scene3_2Controller.cs
+++ Assets/00.TestScenes/SonDaHyeL/Scripts/Editing/Scene3_2Controller.cs
@@ -5,6 +5,8 @@ public class Scene3_2Controller : AllSceneController
 {
     [SerializeField] private Paper paper;
 
+
+
     protected override IEnumerator RunSequence()
     {
         yield return StartCoroutine(soundManager.PlayAndWait("Scene3_2", 0.4f));
